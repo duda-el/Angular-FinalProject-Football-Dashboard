35a1e53f8c844c76646e59be82e53fa362f95c01
Duda Elbakidze <dudaelbakidze94@gmail.com>
2025-01-16 15:18:44 +0400

    made desing update on standings component and fixed routing

--- src/app/pages/standings/standings.component.ts
+++ src/app/pages/standings/standings.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { StandingsService } from '../../services/standings.service';
 import { Standing } from '../../interfaces/standing.interface';
 import { CommonModule } from '@angular/common';
+import { LeftnavComponent } from '../../components/leftnav/leftnav.component';
+import { NavComponent } from '../../components/nav/nav.component';
 
 @Component({
   selector: 'app-standings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LeftnavComponent, NavComponent],
   templateUrl: './standings.component.html',
   styleUrls: ['./standings.component.css'],
 })
